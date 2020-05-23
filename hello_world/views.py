911da3429730af2a8ffbcdbe4a59d88a94f92d70
Basia@users.noreply.github.com <tester@tester-m>
2020-05-23 09:16:13 +0000

    dodanie heroku do travisa

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Basia"
+moje_imie = "Basia to sie udalo"
 msg = "Hello World!"
 
 
