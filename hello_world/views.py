aa899724bd9f47afe64c3d76538c3aa625fd69fa
Basia@users.noreply.github.com <tester@tester-m>
2020-05-23 09:28:39 +0000

    naprawa lint

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Basia to sie udalo"
+moje_imie = "BasiaN"
 msg = "Hello World!"
 
 
