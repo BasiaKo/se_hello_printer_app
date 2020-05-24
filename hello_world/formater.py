d1f0dfa6c2e31ca10e24e08d7b35ee72308c8127
Basia <Basia@users.noreply.github.com>
2020-05-24 12:47:04 +0000

    output xml i json

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,10 +1,14 @@
+import json
+import lxml.etree
+import lxml.builder
 
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
+XML = "xml"
 
-SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
+SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON, XML]
 
 
 def get_formatted(msg, imie, format):
@@ -17,12 +21,26 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif format == XML:
+        result = format_to_xml(msg, imie)
     return result
 
 
 def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "mgs":"' +
-            msg + '"}')
+    slownik={"imie":imie, "msg":msg}
+    wynik=json.dumps(slownik)
+    return wynik
+
+
+def format_to_xml(msg, imie):
+    E=lxml.builder.ElementMaker()
+    root=E.greeting
+    imieN=E.imie(imie)
+    msgN=E.msg(msg)
+
+    doc=root(imieN, msgN)
+    wynik=lxml.etree.tostring(doc, pretty_print=True)
+    return wynik
 
 
 def plain_text(msg, imie):
